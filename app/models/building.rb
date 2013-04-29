02b20c4c5fa19679dec012f3fc07ca44a1150f6a
Julian Simioni <julian.simioni@gmail.com>
2013-04-28 23:36:25 -0700

    Fix deprecation warning

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,7 +2,7 @@ class Building < ActiveRecord::Base
   belongs_to :city
   attr_accessible :businessService, :name, :speeds
 
-  scope :ungeocoded, where(latlon: nil)
+  scope :ungeocoded, ->{ where(latlon: nil) }
 
   def address
     "#{name}, #{city.name}, CA"

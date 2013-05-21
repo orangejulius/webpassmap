b895e206900faa5359d627759b4d5298335d8ba0
Julian Simioni <julian.simioni@gmail.com>
2013-05-20 23:45:47 -0700

    Switch to secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WPMap::Application.config.secret_token = 'aed24606004dd90838a9690e2daec75c21e6828fa737ef65541c2a5b49b22f959409ae77245a451a25f16568c6d24c7fa8e6e4887de1586165bc614d12fbf425'
+WPMap::Application.config.secret_key_base = 'aed24606004dd90838a9690e2daec75c21e6828fa737ef65541c2a5b49b22f959409ae77245a451a25f16568c6d24c7fa8e6e4887de1586165bc614d12fbf425'

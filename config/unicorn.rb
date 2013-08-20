5c5f1edc5b5f1d8b7b2270b0d3aea769aa913f21
Julian Simioni <julian.simioni@gmail.com>
2013-08-19 20:05:42 -0700

    Add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+# Set your full path to application.
+app_path = "/home/webpassmap/current"
+
+# Set unicorn options
+worker_processes 1
+preload_app true
+timeout 180
+listen "127.0.0.1:9000"
+
+# Spawn unicorn master worker for user apps (group: apps)
+user 'webpassmap', 'webpassmap'
+
+# Fill path to your app
+working_directory app_path
+
+# Should be 'production' by default, otherwise use other env
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# Log everything to one file
+stderr_path "log/unicorn.log"
+stdout_path "log/unicorn.log"

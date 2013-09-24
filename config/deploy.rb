59f4247e32a11a5229075d6230cc36694178a85d
Julian Simioni <julian.simioni@gmail.com>
2013-09-24 13:40:13 -0700

    Setup symlinks during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,17 @@ set :deploy_to, '/home/webpassmap'
 set :use_sudo, false
 
 after 'deploy:restart', 'unicorn:restart'  # app preloaded
+after  'deploy:finalize_update', 'config:symlink'
+
+namespace :config do
+  desc "Symlink the configuration files for the app"
+  task :symlink do
+    run <<-CMD
+      for i in `ls -A #{shared_path}/config`; do
+        if test -f "#{shared_path}/config/$i"; then
+          ln -nfs "#{shared_path}/config/$i" "#{release_path}/config/$i";
+        fi;
+      done;
+    CMD
+  end
+end
